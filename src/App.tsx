0e8094a99d0f56446096b437c804360cfcf10388
Luan Gomes <luangms@outlook.com>
2023-02-09 21:08:07 -0300

    adjusting css and App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,61 @@
-import { useState } from 'react'
 import './global.css'
+import styles from './App.module.css';
+import { Header } from './components/Header';
+import { CreateTask } from './components/CreateTask';
+import { TaskList } from './components/TaskList';
+import { useState } from 'react';
+
+export interface Task {
+  content: string,
+  completed: boolean
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const handleAddNewTask = (content: string) => {
+    setTasks(prevState => {
+      const newTask = {
+        content,
+        completed: false,
+      };
+      const tasksUpdated = [...prevState, newTask];
+      return tasksUpdated
+    });
+  };
+
+  const handleChangeTaskCompletedStatus = (content: string) => {
+    const updatedTaskList = tasks.map(task => {
+      if (task.content === content) {
+        const updatedTask = {
+          completed: task.completed === true ? false : true,
+          content: task.content,
+        }
+        return updatedTask;
+      }
+      return task;
+    })
+    setTasks(updatedTaskList);
+  };
+
+  const handleDeleteTask = (content: string) => {
+    const newTasksList = tasks.filter(task => {
+      return task.content !== content;
+    })
+    setTasks(newTasksList);
+  };
 
   return (
-    <div className="App">
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <CreateTask handleAddNewTask={handleAddNewTask} />
+        <TaskList
+          tasks={tasks}
+          handleChangeTaskCompletedStatus={handleChangeTaskCompletedStatus}
+          handleDeleteTask={handleDeleteTask}
+        />
+      </div>
     </div>
   )
 }
